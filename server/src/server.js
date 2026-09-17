50eded0ac512f04e2939981d4858e322bb8eebad
varshith1024 <blackpantherpc2@gmail.com>
2026-09-17 13:22:37 +0530

    feat: structure Express application

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,8 @@
-const express = require("express");
+require("dotenv").config();
 
-const app = express();
+const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "AlgoQuest API is running 🚀",
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`AlgoQuest server running on port ${PORT}`);
